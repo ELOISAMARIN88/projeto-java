b71271c7bf9636c0c8bc2f52ee8e5da74c09ffbf
ELOISAMARIN88 <132958680+ELOISAMARIN88@users.noreply.github.com>
2023-12-22 14:45:03 -0300

    Add files via upload
    Adicionando Class Pagamento do projeto

---
+++ Pagamento.java
@@ -0,0 +1,59 @@
+package Pagamento;
+
+public abstract class Pagamento {
+	
+	String nomedoCliente;
+	private int CPF;
+	private int RG;
+	
+	
+	public Pagamento(String nomedoCliente, int cPF, int rG) {
+		super();
+		this.nomedoCliente = nomedoCliente;
+		CPF = cPF;
+		RG = rG;
+	}
+
+	public String getNomedoCliente() {
+		return nomedoCliente;
+	}
+
+	public void setNomedoCliente(String nomedoCliente) {
+		this.nomedoCliente = nomedoCliente;
+	}
+
+	public int getCPF() {
+		return CPF;
+	}
+
+	public void setCPF(int cPF) {
+		CPF = cPF;
+	
+	}
+
+	public int getRG() {
+		return RG;
+	}
+
+	public void setRG(int rG) {
+		RG = rG;
+	}
+	
+	public void visualizar() {
+		
+		String formadePagamento = "";
+		
+		System.out.println("\n\n-------------------------------------------------");
+		System.out.println("Nota de Pagamento: ");
+		System.out.println("-----------------------------------------------------");
+		System.out.println("Nome do Cliente: " + this.nomedoCliente );
+		System.out.println("CPF do Cliente: " + this.CPF);
+		System.out.println("RG do Cliente: " + this.RG);
+		System.out.println("\nForma de Pagamento: " + formadePagamento);
+		
+	}
+	
+	
+	
+
+}
